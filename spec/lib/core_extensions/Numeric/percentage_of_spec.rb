0054b59f7f34b83f75c193e9e67d8e53701e2e4d
Ernie Brodeur <ebrodeur@ujami.net>
2014-10-26 17:01:38 -0700

    Added a spec for Numeric#percentage_of

---
+++ spec/lib/core_extensions/Numeric/percentage_of_spec.rb
@@ -0,0 +1,14 @@
+require 'core_augmentations/Numeric/percentage_of'
+
+describe Numeric do
+  describe "#percentage_of" do
+    # Goddamn, some days.  I really need to figure out how to phrase this better.
+    it "Given an argument, return the percentage of the number in self." do
+      expect(10.percentage_of(100)).to eq 10
+      expect(10.percentage_of(990)).to eq 1.01
+    end
+    it "Will round given a round: parameter." do
+      expect(10.percentage_of(667281,5)).to eq 0.0015
+    end
+  end
+end
