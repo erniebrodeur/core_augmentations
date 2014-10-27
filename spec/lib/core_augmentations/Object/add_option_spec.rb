fdcabd3ed11e7eeb156a29c95dbcf0bc5715b38f
Ernie Brodeur <ebrodeur@ujami.net>
2014-10-27 08:28:47 -0700

    Object#add_option will now return an array of added options.

--- spec/lib/core_augmentations/Object/add_option_spec.rb
+++ spec/lib/core_augmentations/Object/add_option_spec.rb
@@ -20,5 +20,11 @@ describe Object do
         expect(@test_object.test).to eq t
       end
     end
+    it "should return the list of options as an array when given no argument" do
+      @test_object = Array.new
+      @test_object.add_option('test')
+      @test_object.add_option(:test2)
+      expect(@test_object.add_option).to eq [:test, :test2]
+    end
   end
 end
